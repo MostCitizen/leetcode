e51db3c4a0a6cf63efdf958ce0fff5f8456b6be6
LeeHoYun <86865572+MostCitizen@users.noreply.github.com>
2026-06-07 16:33:31 +0900

    Time: 14 ms (12.66%), Space: 13.2 MB (52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,57 +2,25 @@ class Solution {
 public:
     int romanToInt(string s) {
         int result = 0;
-        for(int i=0;i<s.size();i++){
-            char c = s[i];
-            if(c == 'I'){
-                int num = 1;
-                if(s[i+1] == 'V'){
-                    num = 4;
-                    i++;
-                }
-                else if(s[i+1] == 'X'){
-                    num = 9;
-                    i++;
-                }
-                result += num;
+        int n = s.size();
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        for(int i=0;i<n-1;i++){
+            if(roman[s[i]] < roman[s[i+1]]){
+                result -= roman[s[i]];
             }
-            else if(c == 'V'){
-                result += 5;
-            }
-            else if(c == 'X'){
-                int num = 10;
-                if(s[i+1] == 'L'){
-                    num = 40;
-                    i++;
-                }
-                else if(s[i+1] == 'C'){
-                    num = 90;
-                    i++;
-                }
-                result += num;
-            }
-            else if(c == 'L'){
-                result += 50;
-            }
-            else if(c == 'C'){
-                int num = 100;
-                if(s[i+1] == 'D'){
-                    num = 400;
-                    i++;
-                }
-                else if(s[i+1] == 'M'){
-                    num = 900;
-                    i++;
-                }
-                result += num;
-            }
-            else if(c == 'D'){
-                result += 500;
-            }
-            else if(c == 'M'){
-                result += 1000;
+            else{
+                result += roman[s[i]];
             }
         }
-        return result;
+        return result + roman[s[n-1]];
     }
 };
