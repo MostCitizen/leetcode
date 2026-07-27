32dd6abffb79a390c8898a4f850406696ff6ef86
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-07-27 19:01:29 +0900

    Time: 0 ms (100%), Space: 152.2 MB (6.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int rob(List<int> nums) {
+    int size = nums.length;
+    if(size == 1) return nums[0];
+    List<int> dp = List.filled(size, 0);
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for(int i=2;i<size;i++){
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+    }
+    return dp[size-1];
+  }
+}
