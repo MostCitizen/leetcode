26c2a94274dd616acd6c39c93042c5d47a6c3900
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-07-27 17:27:06 +0900

    Time: 1 ms (21.74%), Space: 147.9 MB (60.87%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.dart
+++ 0118-pascals-triangle/0118-pascals-triangle.dart
@@ -1,12 +1,10 @@
 class Solution {
   List<List<int>> generate(int numRows) {
-    List<List<int>> list = [];
-    for(int i=0;i<numRows;i++){
-        list.add(List.filled(i + 1, 0));
-        list[i][0] = 1;
-        list[i][i] = 1;
-    }
-    for(int i=2;i<numRows;i++){
+    List<List<int>> list = [
+        [1]
+    ];
+    for(int i=1;i<numRows;i++){
+        list.add(List.filled(i + 1, 1));
         for(int j=1;j<i;j++){
             list[i][j] = list[i-1][j-1] + list[i-1][j];
         }
