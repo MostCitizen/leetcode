2c9e7aaa4002d5f01af62008a71f0a26581eb398
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-08-04 16:17:10 +0900

    Time: 0 ms (100%), Space: 32.9 MB (39.17%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int> v;
-        int mn = *min_element(nums.begin(), nums.end());
-        int mx = *max_element(nums.begin(), nums.end());
+        int mn = INT_MAX;
+        int mx = -INT_MAX;
+        int arr[101] = {0,};
+
+        for(int num : nums){
+            arr[num] = 1;
+            mn = min(mn, num);
+            mx = max(mx, num);
+        }
         for(int i=mn;i<mx;i++){
-            if(find(nums.begin(), nums.end(), i) == nums.end()) v.push_back(i);
+            if(!arr[i]) v.push_back(i);
         }
         return v;
     }
