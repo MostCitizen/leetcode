47b1761724a675bada8688d12d9bc93ed0930add
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-08-04 17:36:09 +0900

    Time: 11 ms (24.49%), Space: 47.5 MB (6.83%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,8 +3,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i+=2){
             if(nums[i] != nums[i+1]){
-                if(i == 0 || nums[i-1] != nums[i]) return nums[i];
-                else return nums[i+1];
+                return nums[i];
             }
         }
         return nums[nums.length-1];
