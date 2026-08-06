0d8a55bdf6dc70ae87a07bd82176d8044902f92f
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-08-06 19:41:15 +0900

    Time: 0 ms (100%), Space: 59.1 MB (89.55%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    return isValid(root, Number.MAX_SAFE_INTEGER, -Number.MAX_SAFE_INTEGER)
+};
+
+function isValid(root: TreeNode | null, max:number, min:number){
+    if(root == null) return true;
+    else if(root.val <= min || root.val >= max) return false;
+    return isValid(root.left, root.val, min) && isValid(root.right, max, root.val);
+}
