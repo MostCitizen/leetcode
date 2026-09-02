52fb9cd39b24014a5db7202c61bf8172854885d4
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-09-02 21:44:55 +0900

    Time: 0 ms (100%), Space: 11.6 MB (60.88%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,25 +2,16 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        if(digits[n-1] != 9) {
-            digits[n-1]+=1;
-            return digits;
-        }
-        int carry = 1;
-        digits[n-1] = 0;
 
-        for(int i=n-2;i>=0;i--){
-            if(digits[i] + 1 > 9){
-                carry = 1;
-                digits[i] = 0;
-            }else{
-                carry = 0;
-                digits[i]++;
-                break;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] + 1 != 10){
+                digits[i]+=1;
+                return digits;
             }
-        }
-        if(carry){
-            digits.insert(digits.begin(), 1);
+            digits[i] = 0;
+            if(i == 0){
+                digits.insert(digits.begin(), 1);
+            }
         }
         return digits;
     }
