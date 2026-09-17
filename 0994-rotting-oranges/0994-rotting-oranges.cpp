46e4564188e0f797858123aa419dfff2d9164914
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-09-17 17:17:05 +0900

    Time: 3 ms (25.68%), Space: 16.7 MB (86.02%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int total = 0;
-        queue<pair<int,int>> q;
-        int row = grid.size();
-        int col = grid[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j] == 1){
-                    total++;
+                    fresh++;
                 }else if(grid[i][j] == 2){
                     q.push({i, j});
                 }
             }
         }
+        int dr[] = {1, -1, 0, 0};
+        int dc[] = {0, 0, 1, -1};
         int count = 0;
         while(!q.empty()){
             int size = q.size();
-            int dr[] = {1, -1, 0, 0};
-            int dc[] = {0, 0, 1, -1};
             bool isChange = false;
-            while(size--){
-                auto [r,c] = q.front();
+            for(int i=0;i<size;i++){
+                auto [r, c] = q.front();
                 q.pop();
-                for(int i=0;i<4;i++){
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-                    if(nr < 0 || nr >= row || nc < 0 || nc >= col){
-                        continue;
-                    }else if(grid[nr][nc] != 1){
-                        continue;
+                for(int j=0;j<4;j++){
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
+                    if(nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                    if(grid[nr][nc] == 1){
+                        isChange = true;
+                        grid[nr][nc] = 2;
+                        q.push({nr, nc});
+                        fresh--;
                     }
-                    total--;
-                    isChange = true;
-                    grid[nr][nc] = 2;
-                    q.push({nr, nc});
                 }
             }
             if(isChange) count++;
         }
-        return total != 0 ? -1 : count;
+        return fresh ? -1 : count;
     }
 };
