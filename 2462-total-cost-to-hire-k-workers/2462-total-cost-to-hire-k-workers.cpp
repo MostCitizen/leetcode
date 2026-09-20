bc63bc51a0a52db7d92b14081383881d09aa241f
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-09-20 15:11:59 +0900

    Time: 31 ms (93.5%), Space: 76.8 MB (58.59%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        long long sum = 0;
-        int size = costs.size();
-        priority_queue<int, vector<int>, greater<int>> left, right;
-        int lIndex = 0, rIndex = size-1;
-        for(int i=0;i<k;i++){
-            while(left.size() < candidates && lIndex <= rIndex) {
-                left.push(costs[lIndex++]);
+        int n = costs.size();
+        if(n == 1) return costs[0];
+        long long res = 0;
+        priority_queue<int, vector<int>, greater<int>> leftQ, rightQ;
+        bool isMore = true;
+        int left = 0, right = 0;
+        
+        for(int i=0;i<candidates;i++){
+            left = i;
+            right = n - 1 - i;
+            if(left >= right) {
+                if(left == right)
+                    leftQ.push(costs[left++]);
+                isMore = false;
+                break;
             }
-            while(right.size() < candidates && lIndex <= rIndex) {
-                right.push(costs[rIndex--]);
-            }
-            int lt = left.size() > 0 ? left.top() : 1e5;
-            int rt = right.size() > 0 ? right.top() : 1e5;
-            if(lt <= rt){
-                sum += lt;
-                left.pop();
-            }
-            else {
-                sum += rt;
-                right.pop();
-            }
+            leftQ.push(costs[left++]);
+            rightQ.push(costs[right--]);
         }
-        return sum;
+        for(int i=0;i<k;i++){
+            int cost = 0;
+            if(rightQ.empty() || (!leftQ.empty() && leftQ.top() <= rightQ.top())){
+                cost = leftQ.top();
+                leftQ.pop();
+                if(isMore){
+                    leftQ.push(costs[left++]);
+                }
+            }else {
+                cost = rightQ.top();
+                rightQ.pop();
+                if(isMore){
+                    rightQ.push(costs[right--]);
+                }
+            }
+            if(left > right) isMore = false;
+
+            res += cost;
+        }
+        return res;
     }
 };
