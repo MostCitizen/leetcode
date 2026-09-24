92e592b6610cfa6e832242554ed754a186d1ec8b
LeeHoYoon <86865572+MostCitizen@users.noreply.github.com>
2026-09-24 21:07:30 +0900

    Time: 822 ms (22.17%), Space: 11.7 MB (29.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(find(board, word, 0, i, j)){
-                    return true;
+        int m = board.size();
+        int n = board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == word[0]){
+                    bool res = find(board, word, i, j, 0);
+                    if(res) return res;
                 }
             }
         }
         return false;
     }
-
-    bool find(vector<vector<char>>& board, string word, int k, int row, int col){
-        if(word.size() == k){
-            return true;
-        }
-        if(row >= board.size() || row<0 || 
-           col >= board[0].size() || col < 0 || 
-           board[row][col] != word[k]){
-            return false;
-        }
-        char temp = board[row][col];
-        board[row][col] = '\0';
-        bool res = find(board, word, k + 1, row+1, col) || 
-               find(board, word, k + 1, row, col+1) || 
-               find(board, word, k + 1, row-1, col) || 
-               find(board, word, k + 1, row, col-1);
-        board[row][col] = temp;
+    bool find(vector<vector<char>>& board, string word, int r, int c, int index){
+        if(r < 0 || r >= board.size() || 
+            c < 0 || c >= board[0].size()) return false;
+        else if(board[r][c] != word[index]) return false;
+        else if(index == word.size()-1) return true;
+        board[r][c] = '\0';
+        bool res = find(board, word, r+1, c, index+1) || 
+                find(board, word, r-1, c, index+1) ||
+                find(board, word, r, c+1, index+1) ||
+                find(board, word, r, c-1, index+1);
+        board[r][c] = word[index];
         return res;
     }
 };
